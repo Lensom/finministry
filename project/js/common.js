5d28aa3add7ee910f335600b6e4381d146676bde
Lensom <magurawork@gmail.com>
2019-06-25 16:50:18 +0300

    add full page without buttons and owl

--- project/js/common.js
+++ project/js/common.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+    // WOW ANIMATION
+    new WOW().init();
 
-  
+    // OWL CAROUSEL
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        dots: true,
+        items: 2,
+        loop: true,
+        margin: 60,
+        rewindNav: false,
+        responsiveClass: true,
+        autoWidth:true,
+        // responsive:{
+        //     600:{
+        //         items:4
+        //     }
+        // }
+    });
 
 });
 
